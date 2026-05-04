8c0dbe5a3979a38f92ad47f15586792cad6dcde3
Mārtiņš Lazdāns <marrtins@dqdp.net>
2026-05-04 16:03:05 +0300

    Bump php-8.5 version

--- build_scripts/php-fb-build-all.bat
+++ build_scripts/php-fb-build-all.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "phps="php-7.4.13 vc15" "php-8.0.30 vs16" "php-8.1.33 vs16" "php-8.2.29 vs16" "php-8.3.26 vs16" "php-8.4.13 vs17" "php-8.5.0RC2 vs17""
+set "phps="php-7.4.13 vc15" "php-8.0.30 vs16" "php-8.1.33 vs16" "php-8.2.29 vs16" "php-8.3.26 vs16" "php-8.4.13 vs17" "php-8.5.5 vs17""
 
 setlocal enabledelayedexpansion
 for %%p in (%phps%) do (
